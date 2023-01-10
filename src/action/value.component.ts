7c9af87bcb78b1a12ab5f0eb5265108e3918348c
Jose Duran <jose.daniel.duran.toro@gmail.com>
2023-01-09 21:33:04 -0500

    add button to save to file and close the tab

--- src/action/value.component.ts
+++ src/action/value.component.ts
@@ -15,4 +15,24 @@ export class ValueComponent {
     navigator.clipboard.writeText(this.value);
     this.snackBar.open('Copied to clipboard', undefined, { duration: 1000 });
   }
+  download() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(queryOptions, ([tab]) => {
+      let downloadId;
+
+      function onChanged(delta: chrome.downloads.DownloadDelta) {
+        if (delta.id === downloadId && delta.state && delta.state.current !== 'in_progress') {
+          chrome.downloads.onChanged.removeListener(onChanged);
+          chrome.tabs.remove(tab.id);
+        }
+      };
+      chrome.downloads.onChanged.addListener(onChanged);
+
+      let jsonBlob = new Blob( [this.value], { type : "application/json" });
+      let objectURL = window.URL.createObjectURL(jsonBlob);
+      chrome.downloads.download({ url: objectURL, filename: "saml.txt", saveAs: null }, (id) => {
+        downloadId = id;
+      });
+    });
+  }
 }
